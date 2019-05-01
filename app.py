378010920cd8eab2fe53cd52f584377b972bebd6
wx_33a25b54b3aa477689dfeb352459e5a2 <wx_33a25b54b3aa477689dfeb352459e5a2@git.code.tencent.com>
2019-05-01 11:44:20 +0800

    root

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
-from flask import request
-from flask_api import FlaskAPI
-
-app = FlaskAPI(__name__)
+from flask import Flask
+app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def example():
-    if request.method == 'POST':
-        return {'post': request.data}
-    return {'get': 'kiko'}
-
-
-if __name__ == '__main__':
-    app.run()
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
